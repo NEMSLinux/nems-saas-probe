fd9b383e406b83281476275eff5d23ff546d72b3
NEMS Linux <82971297+NEMSLinux@users.noreply.github.com>
2022-09-13 20:21:00 -0400

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal EnableExtensions
+SET SCRIPT_PATH=%~dp0
+
+:uniqLoop
+set "tmpdir=%tmp%\nems-saas~%RANDOM%"
+if exist "%tmpdir%" goto :uniqLoop
+
+md %tmpdir%
+cp src\nems-saas-probe %tmpdir%\
+cd %tmpdir%
+python -m PyInstaller --onefile nems-saas-probe
+
+move dist\nems-saas-probe.exe %SCRIPT_PATH%\windows\
+
+del /r %tmpdir%
